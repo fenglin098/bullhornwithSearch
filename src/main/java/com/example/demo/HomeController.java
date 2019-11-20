bd33e98c4a9cc772843bc4a54ba928ff0854909d
fenglin098 <feng.lin098@gmail.com>
2019-11-20 15:11:14 -0500

    with search function

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -55,5 +52,9 @@ public class HomeController {
         return "redirect:/";
     }
 
-
+    @PostMapping("/searchlist")
+    public String search(Model model, @RequestParam("search") String search){
+        model.addAttribute("messages", messageRepository.findByContentContainingIgnoreCaseOrTitleContainingIgnoreCase(search, search));
+        return "searchlist";
+    }
 }
