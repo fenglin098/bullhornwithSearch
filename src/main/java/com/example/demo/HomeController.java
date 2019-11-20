4d8cb9c6abe6956804b09e2a0f454122c94f73bd
fenglin098 <feng.lin098@gmail.com>
2019-11-20 15:58:25 -0500

    search and update function works

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,17 +1,24 @@
 package com.example.demo;
 
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.util.Map;
 
 @Controller
 public class HomeController {
     @Autowired
     MessageRepository messageRepository;
+
+    @Autowired
+    CloudinaryConfig cloudc;
 
     @RequestMapping("/")
     public String listMessages(Model model){
@@ -26,12 +33,24 @@ public class HomeController {
     }
 
     @PostMapping("/process")
-    public String processForm(@Valid Message message, BindingResult result){
+    public String processForm(@Valid Message message, BindingResult result, @RequestParam("file") MultipartFile file){
         if (result.hasErrors()){
             return "messageform";
         }
-        messageRepository.save(message);
-        return "redirect:/";
+        if (file.isEmpty()){
+            messageRepository.save(message);
+            return "redirect:/";
+        }
+        try {
+            Map uploadResult = cloudc.upload(file.getBytes(),
+                    ObjectUtils.asMap("resourcetype", "auto"));
+            message.setPic(uploadResult.get("url").toString());
+            messageRepository.save(message);
+        }catch (IOException e){
+            e.printStackTrace();
+            return "redirect:/add";
+        }return "redirect:/";
+
     }
 
     @RequestMapping("/detail/{id}")
